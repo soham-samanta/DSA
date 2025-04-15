d3133024642fa4411c67fb57537b0080135c6e0e
soham <sohamsamanta2@gmail.com>
2025-04-15 22:53:21 +0530

    LL asgn

--- SohamCodes/src/LinkList.java
+++ SohamCodes/src/LinkList.java
@@ -10,6 +10,8 @@ class node<E>{
 }
 */
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
 class node{
@@ -279,9 +281,79 @@ public class LinkList {
         return bh.next;
     }
 
+    public static node revK(node head, int k){
+        if(k<=1 || head==null) return head;
+        node prev=null;
+        node curr=head;
+        int len = len(head);
+        int cnt = len/k;
 
-    // homework + Assignment
+        while(cnt>0){
+            node nxt=curr.next;
+            node last = prev;
+            node newEnd = curr;
 
+            for (int i = 0; i < k; i++) {
+                curr.next=prev;
+                prev=curr;
+                curr=nxt;
+                if(nxt!=null){
+                    nxt=nxt.next;
+                }
+            }
+
+            if(last!=null){
+                last.next=prev;
+            }else{
+               head=prev;
+            }
+
+            newEnd.next=curr;
+            prev=newEnd;
+
+            cnt--;
+        }
+        return head;
+    }
+
+    static int longestPalindrome(node head){
+        if(head==null) return 0;
+        if(head.next==null) return 1;
+        int ans = 0;
+
+        node prev=null,curr=head;
+        while(curr!=null){
+            node nxt=curr.next;
+            curr.next=prev;
+            int cntIfCenter = cntCommon(prev,nxt);
+            int lenIfCenter = cntIfCenter*2+1;
+            int cntIfNotCenter = cntCommon(curr,nxt);
+            int lenIfNotCenter = cntIfNotCenter*2;
+
+            int lenFinal = Math.max(lenIfCenter,lenIfNotCenter);
+            ans = Math.max(ans,lenFinal);
+
+            prev=curr;
+            curr=nxt;
+        }
+        return ans;
+    }
+    static int cntCommon(node a , node b){
+        int cnt=0;
+        while(a!=null && b!=null){
+            if(a.val==b.val){
+                cnt++;
+                a=a.next;
+                b=b.next;
+            }else{
+                break;
+            }
+        }
+        return cnt;
+    }
+
+
+    // homework
     public static int kthNode(node head, int b){
         int n = len(head);
         int midN = (n/2)+1;
@@ -314,6 +386,186 @@ public class LinkList {
         return null;
     }
 
+    // Assignment
+    public node sortBinaryList(node head){
+        int cnt0 = 0;
+        node curr=head;
+        while(curr!=null){
+            if(curr.val==0){
+                cnt0++;
+            }
+            curr=curr.next;
+        }
+        node temp=head;
+        while(temp!=null){
+            if(cnt0>0){
+                temp.val=0;
+                cnt0--;
+            }else{
+                temp.val=1;
+            }
+            temp=temp.next;
+        }
+        return head;
+    }
+
+    public node removeNthFromEnd(node head, int k){
+        if(head==null) return head;
+        if(k<1) return head;
+
+        int len=len(head);
+        int pos = len-k;
+
+        if(k>=len){
+            return head.next;
+        }
+
+        node curr=head;
+        for (int i = 0; i < pos-1; i++) {
+            curr=curr.next;
+        }
+        curr.next=curr.next.next;
+
+        return head;
+    }
+
+    public static int findIntersection(node a, node b){
+        int l1=len(a);
+        int l2=len(b);
+        int d=0;
+        node p1=new node(-1);
+        node p2=new node(-1);
+
+        if(l1>l2){
+            d=l1-l2;
+            p1=a;
+            p2=b;
+        }else{
+            d=l2-l1;
+            p1=b;
+            p2=a;
+        }
+        for (int i = 0; i < d; i++) {
+            p1=p1.next;
+        }
+        while(p1!=null && p2!=null){
+            if(p1==p2){
+                return p1.val;
+            }
+            p1=p1.next;
+            p2=p2.next;
+        }
+        return -1;
+    }
+
+    public node getIntersectionNodeOP(node a, node b) { //not OP - just other way
+        Set<node> set = new HashSet<>();
+        node p1 = a;
+        while(p1 != null){
+            set.add(p1);
+            p1 = p1.next;
+        }
+        node p2 = b;
+        while(p2 != null){
+            if(set.contains(p2))
+                return p2;
+            p2 = p2.next;
+        }
+        return null;
+    }
+
+    public static node mergeTwoLists(node a, node b){
+        if(a==null) return b;
+        if(b==null) return a;
+
+        node dummy = new node(-1);
+        node temp=dummy;
+        while(a!=null && b!=null){
+            if(a.val<b.val){
+                temp.next=a;
+                a=a.next;
+            }else{
+                temp.next=b;
+                b=b.next;
+            }
+            temp=temp.next;
+        }
+
+        while(a!=null){
+            temp.next=a;
+            temp=temp.next;
+            a=a.next;
+        }
+        while(b!=null){
+            temp.next=b;
+            temp=temp.next;
+            b=b.next;
+        }
+        return dummy.next;
+    }
+
+    public node swapPairs(node head) {
+        for(node i=head; i!=null && i.next!=null;i=i.next.next){
+            int temp=i.val;
+            i.val=i.next.val;
+            i.next.val=temp;
+        }
+        return head;
+    }
+
+    public static node removeDupII(node head){ //completely remove dup
+        node dummy=new node(-1);
+        dummy.next=head;
+        node prev=dummy;
+        while(head!=null){
+            if(head.next!=null && head.val==head.next.val){
+                while(head.next!=null && head.val==head.next.val){
+                    head=head.next;
+                }
+                prev.next=head.next;
+            }else{
+                prev=prev.next;
+            }
+            head=head.next;
+        }
+        return dummy.next;
+    }
+
+
+    public node evenReverse(node head) {
+        if (head == null || head.next == null) return head;
+
+        node evenHead = null;  // Will store even-positioned nodes
+
+        node curr = head;
+        int pos = 1;
+
+        // Step 1: Extract even-positioned nodes and push them to front (reverse)
+        while (curr != null && curr.next != null) {
+            node evenNode = curr.next;
+            curr.next = evenNode.next; // Remove even node from original list
+
+            evenNode.next = evenHead; // Push to front of even list (reversed)
+            evenHead = evenNode;
+
+            curr = curr.next; // Move two steps
+            pos += 2;
+        }
+
+        // Step 2: Merge reversed even list back into even positions
+        curr = head;
+        while (evenHead != null) {
+            node temp = evenHead;
+            evenHead = evenHead.next;
+
+            temp.next = curr.next;
+            curr.next = temp;
+
+            curr = curr.next.next; // Skip to next original odd-positioned node
+        }
+
+        return head;
+    }
 
 
 
